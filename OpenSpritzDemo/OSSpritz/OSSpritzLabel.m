87c314409022b0f66dfdd5f34d753e0e320c8336
Kevin Sullivan <sideslapd@gmail.com>
2014-05-21 19:48:32 -0400

    Kinda Broken
    Should have been committing

--- OpenSpritzDemo/OSSpritz/OSSpritzLabel.m
+++ OpenSpritzDemo/OSSpritz/OSSpritzLabel.m
@@ -12,6 +12,7 @@
 #define kInterspace 2.0f
 #define maxWordLength 18
 #define pivotChar 9
+#define guideHeight 9
 
 @implementation OSSpritzLabel {
     UIFont *font;
@@ -44,49 +45,126 @@
 {
     font = [UIFont fontWithName:@"Menlo-Bold" size:20.0f];
     float charWidth = [@"m" sizeWithAttributes:@{NSFontAttributeName:font}].width;
+    
+    // Draw guidelines first so they're on the bottom
+    [self drawGuideLines];
+    
     for (int i = 0; i < 18; i++)
     {
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(i*(charWidth+kInterspace), 0, charWidth, self.frame.size.height)];
         
         label.textAlignment = NSTextAlignmentCenter;
         label.font = font;
-        label.backgroundColor = [UIColor clearColor];
-        if (i==pivotChar) label.textColor = [UIColor redColor];
+        label.backgroundColor = [UIColor redColor]; // HIT - individual letter background color (needs to match field bg color)
+        if (i==pivotChar) label.textColor = [UIColor blueColor]; // HIT - this is now set in the for loop below
         label.alpha = 0;
         label.text = @"m";
         [self addSubview:label];
     }
+    
     self.wordsPerMinute = 250;
     labelViews = [self.subviews copy];
     currentWord = 0;
     currentChar = 0;
-    [self drawGuideLines];
+    
+    // HIT -- color all label views properly including the pivot char
+    for (int i = 0; i < [labelViews count]; i++)
+    {
+        if (i == pivotChar)
+            continue;
+        
+        UILabel *label = ((UILabel*)labelViews[i]);
+        
+        label.textColor = [UIColor greenColor];
+    }
 }
 
 - (void)drawGuideLines
 {
+    float guideX = ((UIView*)labelViews[pivotChar]).center.x;
+    //// Abstracted Attributes
+    UIBezierPath* upperLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0,  0, self.frame.size.width, 1)];
+    UIBezierPath* upperGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, 0, 1, guideHeight)];
+    UIBezierPath* downLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0, self.frame.size.height-1, self.frame.size.width, 1)];
+    UIBezierPath* downGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, self.frame.size.height-1-guideHeight, 1, guideHeight)];
+    
+    NSArray *paths = @[upperLinePath, upperGuidePath, downGuidePath, downLinePath];
+    
+    
+    for (UIBezierPath *path in paths)
     {
-        float guideX = ((UIView*)labelViews[pivotChar]).center.x;
-        float guideHeight = 9;
-        //// Abstracted Attributes
-        UIBezierPath* upperLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0,  0, self.frame.size.width, 1)];
-        UIBezierPath* upperGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, 0, 1, guideHeight)];
-        UIBezierPath* downLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0, self.frame.size.height-1, self.frame.size.width, 1)];
-        UIBezierPath* downGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, self.frame.size.height-1-guideHeight, 1, guideHeight)];
-        
-        NSArray *paths = @[upperLinePath, upperGuidePath, downGuidePath, downLinePath];
-        
-        
-        for (UIBezierPath *path in paths)
-        {
-            CAShapeLayer *line = [CAShapeLayer layer];
-            line.path=path.CGPath;
-            line.fillColor = [UIColor blackColor].CGColor;
-            line.opacity = 1.0;
-            line.strokeColor = nil;
-            [self.layer addSublayer:line];
-        }
+        CAShapeLayer *line = [CAShapeLayer layer];
+        line.path=path.CGPath;
+        line.fillColor = [UIColor blackColor].CGColor;
+        line.opacity = 1.0;
+        line.strokeColor = nil;
+        [self.layer addSublayer:line];
     }
+//    UIBezierPath *boxPath = [UIBezierPath bezierPath];
+//
+//    // Set the starting point of the shape.
+//    [boxPath moveToPoint:CGPointMake(0.0, 0.0)];
+//    
+//    // Draw the lines.
+//    [boxPath addLineToPoint:CGPointMake(self.frame.size.width, 0.0)];
+//    [boxPath addLineToPoint:CGPointMake(self.frame.size.width, self.frame.size.height)];
+//    [boxPath addLineToPoint:CGPointMake(0.0, self.frame.size.height)];
+//    [boxPath closePath];
+//    
+//    // Create a layer from the bezier path
+//    CAShapeLayer *boxLayer = [CAShapeLayer layer];
+//    boxLayer.fillRule = kCAFillRuleEvenOdd;
+//    boxLayer.fillColor = [UIColor redColor].CGColor;
+//    boxLayer.path = boxPath.CGPath;
+//    
+//    // Add the layer to the window
+//    [self.layer addSublayer:boxLayer];
+//    
+////    // Determine the center of the pivot character
+//    float guideX = ((UIView*)labelViews[pivotChar]).center.x;
+////
+////    // Create pivot char guide paths
+////    UIBezierPath* upperGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, 0, 1, guideHeight)];
+////    UIBezierPath* lowerGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, self.frame.size.height-1-guideHeight, 1, guideHeight)];
+////    
+////    NSArray *paths = @[upperGuidePath, lowerGuidePath];
+////    
+////    // Iterate over the two paths defining attributes and adding them to the window
+////    for (UIBezierPath *path in paths)
+////    {
+////        CAShapeLayer *line = [CAShapeLayer layer];
+////        line.path=path.CGPath;
+////        line.fillColor = [UIColor blueColor].CGColor;
+////        line.opacity = 1.0;
+////        line.strokeColor = nil;
+////        [self.layer addSublayer:line];
+////    }
+//    
+//    
+//    
+//  
+//     //// Abstracted Attributes                                                //x, y width        heaight
+//     UIBezierPath* upperLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0,  0, self.frame.size.width, 1)];
+//     UIBezierPath* upperGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, 0, 1, guideHeight)];
+//     UIBezierPath* downLinePath = [UIBezierPath bezierPathWithRect:CGRectMake(0, self.frame.size.height-1, self.frame.size.width, 1)];
+//     UIBezierPath* downGuidePath = [UIBezierPath bezierPathWithRect:CGRectMake(guideX, self.frame.size.height-1-guideHeight, 1, guideHeight)];
+//  
+//     NSArray *paths = @[upperLinePath, upperGuidePath, downGuidePath, downLinePath];
+//  
+//  
+//     for (UIBezierPath *path in paths)
+//     {
+//         CAShapeLayer *line = [CAShapeLayer layer];
+//         line.path=path.CGPath;
+//         line.fillColor = [UIColor blackColor].CGColor;
+//         line.opacity = 1.0;
+//         line.strokeColor = nil;
+//         [self.layer addSublayer:line];
+//     }
+  
+  
+  
+
 }
 
 - (void)setText:(NSString *)text
@@ -168,19 +246,18 @@
     for (int i = 0; i < [labelViews count]; i++)
     {
         UILabel *label = ((UILabel*)labelViews[i]);
+        
         if (i < offset)
-        {
             label.alpha = 0;
-        }
+        
         else if (i >= offset && i < word.length + offset)
         {
             label.alpha = 1;
             label.text = [word substringWithRange:(NSRange){i-offset,1}];
         }
+        
         else
-        {
             label.alpha = 0;
-        }
     }
 }
 
